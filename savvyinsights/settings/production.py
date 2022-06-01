7b08a8305958dbf480a0d01f90a58f1e968db093
Savvy Insights <greg@savvyinsights.io>
2022-06-01 08:52:00 -0400

    heroku

---
+++ savvyinsights/settings/production.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import, unicode_literals
+from .base import *
+
+import os
+
+import dj_database_url
+
+DEBUG = False
+
+env = os.environ.copy()
+
+# It's a GOOD idea to lock this down. 
+ALLOWED_HOSTS = ['*'] 
+
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECRET_KEY = env['SECRET_KEY']
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
+try:
+    from .local import *
+except ImportError:
+    pass
